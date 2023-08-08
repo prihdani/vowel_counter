d046b8a323ede17e53e8c336507accbdf191c39d
Dani <prihoczki.daniel@gmail.com>
2023-08-08 18:10:46 +0200

    finished all(for real now)

--- src/VowelCounter.java
+++ src/VowelCounter.java
@@ -1,5 +1,25 @@
-public class Main {
+import java.util.Scanner;
+
+public class VowelCounter {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please give me a word/sentence: ");
+        String wordOrSentence = scanner.nextLine();
+
+        int vowelCounter = 0;
+
+        for (int i = 0; i < wordOrSentence.length(); i++) {
+            if(wordOrSentence.charAt(i) == 'a' || wordOrSentence.charAt(i) == 'e' || wordOrSentence.charAt(i) == 'i' || wordOrSentence.charAt(i) == 'o' || wordOrSentence.charAt(i) == 'u'){
+                vowelCounter++;
+            }
+        }
+
+        if(vowelCounter <= 1){
+            System.out.println("There is " + vowelCounter + " vowel in the word/sentence!" );
+        }else{
+            System.out.println("There are " +vowelCounter + " vowels in the word/sentence!");
+        }
+
     }
 }
