26430dbd71d98f91b397f146d6e8117e2c56fa30
Dani <prihoczki.daniel@gmail.com>
2023-08-08 19:02:15 +0200

    re-did the whole thing into a GUI program

--- src/VowelCounter.java
+++ src/VowelCounter.java
@@ -1,25 +1,38 @@
-import java.util.Scanner;
+import javax.swing.*;
 
 public class VowelCounter {
+
+
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Please give me a word/sentence: ");
-        String wordOrSentence = scanner.nextLine();
+        int countVowels;
 
-        int vowelCounter = 0;
+        String wordOrSentence = JOptionPane.showInputDialog(null,"Write a text or a word");
 
-        for (int i = 0; i < wordOrSentence.length(); i++) {
-            if(wordOrSentence.charAt(i) == 'a' || wordOrSentence.charAt(i) == 'e' || wordOrSentence.charAt(i) == 'i' || wordOrSentence.charAt(i) == 'o' || wordOrSentence.charAt(i) == 'u'){
-                vowelCounter++;
+        countVowels= vowelCounter(wordOrSentence);
+
+        if(countVowels == 0){
+            JOptionPane.showMessageDialog(null, "There are no vowels in the word/sentence!");
+        } else if (countVowels == 1) {
+            JOptionPane.showMessageDialog(null,"There is 1 vowel in the word/sentence!");
+        } else {
+            JOptionPane.showMessageDialog(null,"There are " + countVowels + " vowels in the word/sentence!");
+        }
+
+    }
+
+    static int vowelCounter(String something){
+        int counter = 0;
+        something = something.toLowerCase();
+
+        for (int i = 0; i < something.length(); i++) {
+
+            char toName = something.charAt(i);
+
+            if(toName == 'a' || toName == 'e' || toName == 'i' || toName == 'o' || toName == 'u'){
+                counter++;
             }
         }
-
-        if(vowelCounter <= 1){
-            System.out.println("There is " + vowelCounter + " vowel in the word/sentence!" );
-        }else{
-            System.out.println("There are " +vowelCounter + " vowels in the word/sentence!");
-        }
-
+        return counter;
     }
 }
